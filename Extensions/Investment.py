6cc09925c18c3fb5f42461f7679c2559ee5b6b4e
Tim Walsh <timtwalsh@gmail.com>
2020-06-11 00:29:44 +1000

    Implement boilerplate for Investment Minigame

--- Extensions/Investment.py
+++ Extensions/Investment.py
@@ -12,6 +12,14 @@ TICK_RATE = 6  # Default
 emoji_letters_dict = {'A': '🇦', 'B': '🇧', 'C': '🇨', 'D': '🇩', 'E': '🇪', 'F': '🇫', 'G': '🇬', 'H': '🇭', 'I': '🇮',
                       'J': '🇯', 'K': '🇰', 'L': '🇱', 'M': '🇲', 'N': '🇳', 'O': '🇴', 'P': '🇵', 'Q': '🇶', 'R': '🇷',
                       'S': '🇸', 'T': '🇹', 'U': '🇺', 'V': '🇻', 'W': '🇼', 'X': '🇽', 'Y': '🇾', 'Z': '🇿', ' ': '✴'}
+company_names = ['Hand Job Nails', 'Blue Balls Inc', 'Fat Constructions', 'Umbrella Corp', 'Globo Gym',
+                 'Aquisitions Inc.', 'Soylent Corp', 'Globex', 'Initech']
+company_tickers = ['HANDJB', 'BLUBLS', 'FATCNT', 'UMBREL', 'GLBGYM', 'AQUINC' 'SOYLNT', 'GLOBEX', 'INTEC']
+company_volatility = [0.33, 0.33, 0.33, 0.5, 0.5, 0.5, 1, 1, 1]
+company_price = [500, 100, 1000, 1000, 500, 100, 500, 1000, 100]
+company_description = ["Low Risk", "Low Risk", "Low Risk", "Medium Risk", "Medium Risk", "Medium Risk", "High Risk",
+                       "High Risk", "High Risk"]
+base_volatility = 0.025
 
 
 def _default(self, obj):
@@ -27,43 +35,52 @@ class Investment(commands.Cog):
         global TICK_RATE
         TICK_RATE = bot.TICK_RATE
         self.bot = bot
-        self.member_currency = {}
-        # Bonuses - Note: Cumulative - ie 1+0.5+4 = 5;0.5*IDLE_RATE
-        self.IDLE_RATE = 6 / 60 / 60 * TICK_RATE  # (1 per hour)
-        self.VOICE_BONUS = 0.5  # (100% while in a voice channel)
-        self.ACTIVITY_BONUS = 0.5  # (50% while active)
-        self.HAPPY_HOUR_BONUS = 4  # (400% during happy hour)
+        for ticker in company_tickers:
+            self.stock_holders[ticker] = []
         self.time_elapsed = 0
 
-    def get_user_investments(self, user_id=""):
-        if user_id != "":
-            return self.member_currency[user_id]
+    def get_investors(self, ticker=""):
+        if ticker != "":
+            return self.stock_holders[ticker]
         else:
-            print("Error, must specify a user_id")
+            print("Error, must specify a ticker")
             return False
 
-    def remove_user_investment(self, user_id="", amount=0):
-        # Removes currency from user balance, returns true if user has sufficient balance
-        if user_id != "":
-            if self.member_currency[user_id] >= amount:
-                self.member_currency[user_id] -= amount
-                return True
+    def sell_investment(self, user_id="", ticker="", amount=0):
+        """ sells investments gives currency to user balance, returns true if user has sufficient investment """
+        if ticker != "":
+            if user_id != "":
+                if ticker in self.stock_holders:
+                    if user_id in self.stock_holders[ticker]:
+                        if self.stock_holders[ticker][user_id] >= amount:
+                            self.stock_holders[user_id] -= amount
+                            return True
+                        else:
+                            print(f"{user_id} doesn't have {amount} {ticker}.")
+                            return False
             else:
+                print("Error, must specify a user_id.")
                 return False
         else:
-            print("Error, must specify a user_id")
+            print("Error, must specify a ticker.")
             return False
 
-    def add_user_investment(self, user_id="", amount=0):
-        # adds currency from user balance, returns true if user has sufficient balance
-        if user_id != "":
-            if amount > 0:
-                self.member_currency[user_id] += amount
-                return True
+    def buy_investment(self, user_id="", ticker="", amount=0):
+        """ Buys investment, removes currency from user balance, returns true if user has sufficient balance"""
+        if ticker != "":
+            if user_id != "":
+                if ticker in self.stock_holders:  # is it a valid ticker
+                    if user_id in self.stock_holders[ticker]:
+                        current_stock = self.stock_holders[ticker][user_id]
+                    else:
+                        current_stock = 0
+                    user_balance = self.bot.get_cog('Currency').get_user_currency(user_id)
+                    self.stock_holders[ticker][user_id] = 1
             else:
+                print("Error, must specify a user_id.")
                 return False
         else:
-            print("Error, must specify a user_id")
+            print("Error, must specify a ticker.")
             return False
 
     async def load_data(self):
@@ -84,47 +101,21 @@ class Investment(commands.Cog):
             with open(f'{self.qualified_name}_data.json', 'w+') as out_file:
                 json.dump(save_data, out_file, sort_keys=False, indent=4)
 
-    @commands.command(name="mycurrency", aliases=["my$", "my $", "my$hekels", "user$", "user $"])
-    async def mycurrency(self, ctx, *, member: discord.Member = None):
-        """!my$ or user$ [user]"""
+    @commands.command(name="investments", aliases=["myinv", "my inv", "myinvestments", "userinv", "user inv"])
+    async def investments(self, ctx, *, member: discord.Member = None):
+        """!myinv or userinv [user]"""
         member = member or ctx.author
-        currency = self.member_currency[str(member.id)]
-        currency_name = self.bot.CURRENCY_NAME if 2 > currency >= 1 else self.bot.CURRENCY_NAME + 's'
-        msg = f"{member.mention} has {currency:.2f} {currency_name}"
-        log = await self.bot.get_channel(self.bot.LOG_CHANNEL).send(msg)
-        await ctx.send(msg, delete_after=self.bot.MEDIUM_DELETE_DELAY)
-        await ctx.message.delete(delay=self.bot.SHORT_DELETE_DELAY)
+        # currency = self.member_currency[str(member.id)]
+        # currency_name = self.bot.CURRENCY_NAME if 2 > currency >= 1 else self.bot.CURRENCY_NAME + 's'
+        # msg = f"{member.mention} has {currency:.2f} {currency_name}"
+        # log = await self.bot.get_channel(self.bot.LOG_CHANNEL).send(msg)
+        # await ctx.send(msg, delete_after=self.bot.MEDIUM_DELETE_DELAY)
+        # await ctx.message.delete(delay=self.bot.SHORT_DELETE_DELAY)
 
     async def timeout(self):
         debug_channel = self.bot.get_channel(self.bot.DEBUG_CHANNEL)
         log_channel = self.bot.get_channel(self.bot.LOG_CHANNEL)
         if not self.bot.is_closed() and len(self.member_currency) > 0:
-            for member in self.bot.get_all_members():
-                if str(member.id) not in self.member_currency.keys():
-                    print("New Member:", member)
-                    self.member_currency[str(member.id)] = 0.0
-                    current_member_currency = 0.0
-                else:
-                    current_member_currency = self.member_currency[str(member.id)]
-                # Apply Activity Bonuses to encourage member participation
-                cumulative_activity_bonus = 1
-                if member.activity is not None:
-                    cumulative_activity_bonus += self.ACTIVITY_BONUS
-                if member.voice is not None:
-                    cumulative_activity_bonus += self.VOICE_BONUS
-
-                # Apply Happy Hour Bonus, 6pm-midnight on weekdays, all hours on Saturday/Sunday
-                weekday = datetime.today().weekday()
-                hour = datetime.today().hour + datetime.today().minute / 60
-                if weekday >= 5:
-                    cumulative_activity_bonus += self.HAPPY_HOUR_BONUS
-                else:
-                    if 18 <= hour < 24:
-                        cumulative_activity_bonus += self.HAPPY_HOUR_BONUS
-                current_member_currency += self.IDLE_RATE * cumulative_activity_bonus
-                self.member_currency[str(member.id)] = current_member_currency
-                if DEBUG:
-                    print(member.id, member, self.member_currency[str(member.id)], sep=",")
             self.time_elapsed += TICK_RATE
 
 
